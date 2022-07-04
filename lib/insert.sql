a27d1db5e18d1d123d32934ab048caa55a97560f
Devin Garcia <devingarcia5423@gmail.com>
2022-07-04 10:35:42 -0400

    commit tree aaa6608685399d38f8d394cfc0b1bd0685a0caa7

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Mr.Chocolate", 12, "M", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Rowdy", 12, "M", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Tabitha", 12, "F", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Sergeant Brown", 12, "M", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Melissa", 12, "F", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Grinch", 12, "M", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Wendy", 12, "F", "Black", "Angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(null, 12, "M", "Black", "Angry", 1);
